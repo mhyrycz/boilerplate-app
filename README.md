74ba3757ef50ad460fb60c3ca3703578bc50ad91
mhyrycz <maciej.hyrycz@solarwinds.cloud>
2018-12-13 15:54:18 +0100

    select tag created by react component and by pure js code

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # boilerplate-app
 prepared environment with empty react application
-after cloning use ```yarn install``` to add node-modules. To run server ```yarn run dev-server```
+after cloning use ```yarn install``` to add node-modules. to add styled-components ```npm install --save styled-components```. To run server ```yarn run dev-server```
--- src/app.js
+++ src/app.js
@@ -1,6 +1,63 @@
+// Opcja 1
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import Select from 'react-select'
 
-ReactDOM.render(<p>boiler plate</p>, document.getElementById('app'));
+const customStyles = {
+  option: (provided, state) => ({
+   borderBottom: '1px dotted black',
+   color: 'white',
+   padding: 20,
+   background: 'grey',
+  }),
+}
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
+
+class App extends React.Component {
+  state = {
+    selectedOption: null,
+  }
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption });
+  }
+  render() {
+    const { selectedOption } = this.state;
+
+    return (
+      <Select
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={options}
+        styles={customStyles}
+      />
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+// Option 2
+
+var potatoTeam = ['Select Person', 'Dominika', 'Danuta', 'Kamil', 'Bartek', 'Łukasz', 'Tomek', 'Maciek H', 'Maciek Ch', 'Oskar'];
+var createSelect = document.createElement('select');
+document.getElementsByTagName('body')[0].appendChild(createSelect);
+
+for (var i = 0; i < potatoTeam.length; ++i) {
+  var createOption = document.createElement('option');
+
+  if (i == 0) {
+    createOption.setAttribute('disabled', 'disabled');
+    createOption.setAttribute('selected', 'selected');
+  }
+
+  var oTxt = document.createTextNode(potatoTeam[i]);
+  createOption.appendChild(oTxt);
+
+  document.getElementsByTagName('select')[0].appendChild(createOption);
+}
